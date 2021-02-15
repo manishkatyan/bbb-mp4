e9b5cb5010338f5564bbd7c3f3e44ebf83faed94
Manish Katyan <manishkatyan@gmail.com>
2021-02-15 21:25:39 +0530

    Fix chrome update ossie

--- bbb-mp4-install.sh
+++ bbb-mp4-install.sh
@@ -47,6 +47,7 @@ apt-get -y install xvfb
 echo "Installing Google Chrome"
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+apt-get -y update
 apt-get -y install google-chrome-stable
 
 echo "Installing FFmpeg"
